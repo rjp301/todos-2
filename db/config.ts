fb1ebe52febb7a6e16493a1a2bc2928c3758c4a6
Riley Paul <rileypaul96@gmail.com>
2024-06-23 21:49:48 -0600

    add other entities to db

--- db/config.ts
+++ db/config.ts
@@ -1,3 +1,4 @@
+import { weightUnits } from "@/api/lib/weight-units";
 import { NOW, column, defineDb, defineTable } from "astro:db";
 
 const User = defineTable({
@@ -25,15 +26,64 @@ const Item = defineTable({
     id: column.text({ primaryKey: true }),
     userId: column.text({ references: () => User.columns.id }),
     createdAt: column.text({ default: NOW }),
+
     name: column.text({ default: "" }),
     description: column.text({ default: "" }),
     weight: column.number({ default: 0 }),
-    weightUnit: column.text({ default: "g" }),
+    weightUnit: column.text({ default: weightUnits.g }),
     image: column.text({ optional: true }),
+  },
+});
+
+const List = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    userId: column.text({ references: () => User.columns.id }),
+    createdAt: column.text({ default: NOW }),
+
+    name: column.text({ default: "" }),
+    description: column.text({ default: "" }),
+
+    showImages: column.boolean({ default: false }),
+    showPrices: column.boolean({ default: false }),
+    showPacked: column.boolean({ default: false }),
+    showWeights: column.boolean({ default: false }),
+
+    sortOrder: column.number({ default: 0 }),
+    weightUnit: column.text({ default: weightUnits.g }),
+  },
+});
+
+const Category = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    userId: column.text({ references: () => User.columns.id }),
+    listId: column.text({ references: () => List.columns.id }),
+    createdAt: column.text({ default: NOW }),
+
+    name: column.text(),
+    sortOrder: column.number({ default: 0 }),
+  },
+});
+
+const CategoryItem = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    userId: column.text({ references: () => User.columns.id }),
+    categoryId: column.text({ references: () => Category.columns.id }),
+    itemId: column.text({ references: () => Item.columns.id }),
+    createdAt: column.text({ default: NOW }),
+
+    sortOrder: column.number({ default: 0 }),
+    quantity: column.number({ default: 1 }),
+
+    packed: column.boolean({ default: false }),
+    wornWeight: column.boolean({ default: false }),
+    consWeight: column.boolean({ default: false }),
   },
 });
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: { User, UserSession, Item },
+  tables: { User, UserSession, Item, List, Category, CategoryItem },
 });
--- src/api/lib/weight-units.ts
+++ src/api/lib/weight-units.ts
@@ -1,2 +1,16 @@
-export const weightUnits = ["g", "kg", "oz", "lb"] as const;
-export type WeightUnit = (typeof weightUnits)[number];
+const weightUnitOptions = ["g", "kg", "oz", "lb"] as const;
+export type WeightUnit = (typeof weightUnitOptions)[number];
+
+export const weightUnitsFull: Record<WeightUnit, string> = {
+  g: "grams",
+  kg: "kilograms",
+  oz: "ounces",
+  lb: "pounds",
+};
+
+export const weightUnits: Record<WeightUnit, string> = {
+  g: "g",
+  kg: "kg",
+  oz: "oz",
+  lb: "lb",
+};
