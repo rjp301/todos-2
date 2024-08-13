a742bf83966820bab7dda29f560e957dc924702c
Riley Paul <rileypaul96@gmail.com>
2024-08-12 21:42:58 -0600

    abstract confirm delete dialog

---
+++ src/app/components/base/confirm-delete-dialog.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/app/components/ui/alert-dialog";
+
+type Props = {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  handleDelete: () => void;
+  entityName?: string;
+};
+
+const ConfirmDeleteDialog: React.FC<Props> = (props) => {
+  const { isOpen, setIsOpen, handleDelete, entityName } = props;
+
+  return (
+    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            {entityName ?? "data"}.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDelete}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default ConfirmDeleteDialog;
