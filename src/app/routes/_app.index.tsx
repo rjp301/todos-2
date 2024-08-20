62e463f2afdf3f1a3cbc4a18e4cdc4a475fdc79c
Riley Paul <rileypaul96@gmail.com>
2024-08-19 18:12:27 -0600

    add container class to header and body

--- src/app/routes/_app.index.tsx
+++ src/app/routes/_app.index.tsx
@@ -6,11 +6,11 @@ export const Route = createFileRoute("/_app/")({
   component: () => (
     <div className="h-full">
       <AppHeader />
-      <div className="flex justify-center items-center h-full">
-        <div className="flex flex-col gap-2 items-center max-h-[50vh] h-full">
+      <div className="container flex h-full items-center justify-center">
+        <div className="flex h-full max-h-[50vh] flex-col items-center gap-2">
           <HomeIcon size="4rem" className="text-primary" />
           <h2 className="text-2xl font-bold">Welcome to Packlighter</h2>
-          <p className="text-muted-foreground text-sm">
+          <p className="text-sm text-muted-foreground">
             Select a list to get packing
           </p>
         </div>
