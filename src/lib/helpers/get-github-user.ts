b9d92744c8bdaeb2425b5f0f38f070c2975d28bb
Riley Paul <rileypaul96@gmail.com>
2024-09-02 19:59:25 -0600

    update github user helper

--- src/lib/helpers/get-github-user.ts
+++ src/lib/helpers/get-github-user.ts
@@ -1,38 +1,47 @@
-interface GithubUser {
-  id: number;
-  login: string;
-  name: string;
-  avatar_url: string;
-}
+import { z } from "zod";
 
-interface GithubEmail {
-  email: string;
-  primary: boolean;
-  verified: boolean;
-  visibility: string;
-}
+const githubUserSchema = z.object({
+  id: z.number(),
+  login: z.string(),
+  name: z.string(),
+  avatar_url: z.string(),
+});
+
+const githubEmailSchema = z.object({
+  email: z.string(),
+  primary: z.boolean(),
+  verified: z.boolean(),
+  visibility: z.string(),
+});
+const githubEmailsSchema = z.array(githubEmailSchema);
 
 export default async function getGithubUser(accessToken: string) {
   const fetchInit: FetchRequestInit = {
     headers: { Authorization: `Bearer ${accessToken}` },
   };
 
-  const [user, emails] = await Promise.all<
-    [Promise<GithubUser>, Promise<GithubEmail[]>]
-  >([
-    fetch("https://api.github.com/user", fetchInit).then((res) => res.json()),
-    fetch("https://api.github.com/user/emails", fetchInit).then((res) =>
-      res.json(),
-    ),
-  ]);
+  try {
+    const user = await fetch("https://api.github.com/user", fetchInit)
+      .then((res) => res.json())
+      .then(githubUserSchema.parse);
 
-  const primaryEmail = emails.find((email) => email.primary && email.verified);
-  if (!primaryEmail) {
-    throw new Error("Primary email not found");
+    const emails = await fetch("https://api.github.com/user/emails", fetchInit)
+      .then((res) => res.json())
+      .then(githubEmailsSchema.parse);
+
+    const primaryEmail = emails.find(
+      (email) => email.primary && email.verified,
+    );
+    if (!primaryEmail) {
+      throw new Error("Primary email not found");
+    }
+
+    return {
+      ...user,
+      email: primaryEmail.email,
+    };
+  } catch (e) {
+    console.error(e);
+    throw new Error("Failed to fetch user data from GitHub");
   }
-
-  return {
-    ...user,
-    email: primaryEmail.email,
-  };
 }
