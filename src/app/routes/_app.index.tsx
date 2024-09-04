180effef66b7fdc8b92ad586e3a67009c2b2ba71
Riley Paul <rileypaul96@gmail.com>
2024-09-04 16:01:41 -0600

    add create new list button to home page

--- src/app/routes/_app.index.tsx
+++ src/app/routes/_app.index.tsx
@@ -1,9 +1,13 @@
 import AppHeader from "@/app/components/app-header";
 import { createFileRoute } from "@tanstack/react-router";
-import { HomeIcon } from "lucide-react";
+import { HomeIcon, Plus } from "lucide-react";
+import { Button } from "../components/ui/button";
+import useMutations from "../hooks/use-mutations";
+import { Separator } from "../components/ui/separator";
 
-export const Route = createFileRoute("/_app/")({
-  component: () => (
+const Component: React.FC = () => {
+  const { addList } = useMutations();
+  return (
     <div className="h-full">
       <AppHeader />
       <div className="container2 flex h-full items-center justify-center">
@@ -13,8 +17,21 @@ export const Route = createFileRoute("/_app/")({
           <p className="text-sm text-muted-foreground">
             Select a list to get packing
           </p>
+          <Separator className="relative my-4">
+            <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform bg-background px-2 text-xs text-muted-foreground">
+              OR
+            </span>
+          </Separator>
+          <Button variant="default" onClick={() => addList.mutate({})}>
+            <Plus className="mr-2 size-4" />
+            <span>Create a new list</span>
+          </Button>
         </div>
       </div>
     </div>
-  ),
+  );
+};
+
+export const Route = createFileRoute("/_app/")({
+  component: Component,
 });
