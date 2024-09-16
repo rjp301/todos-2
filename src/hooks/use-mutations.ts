5198f061ea34f1b7656af28fb38a8efe5f3ac154
Riley Paul <rileypaul96@gmail.com>
2024-09-15 19:18:13 -0600

    addd use mutation helper functions

--- src/hooks/use-mutations.ts
+++ src/hooks/use-mutations.ts
@@ -1,78 +1,36 @@
-import {
-  useMutation,
-  useQueryClient,
-  type QueryKey,
-  type Updater,
-} from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import {
   itemsQueryOptions,
   listQueryOptions,
   listsQueryOptions,
   otherListCategoriesQueryOptions,
 } from "@/lib/queries";
-import { toast } from "sonner";
 import {
   type ExpandedList,
   type ExpandedCategory,
   type ExpandedCategoryItem,
   type ListSelect,
 } from "@/lib/types";
-import React from "react";
 import { produce } from "immer";
 import { initCategory, initCategoryItem, initItem } from "@/lib/init";
 import { actions } from "astro:actions";
 import { useNavigate } from "react-router-dom";
 import useCurrentList from "./use-current-list";
+import useMutationHelpers from "./use-mutation-helpers";
 
 export default function useMutations() {
   const { listId } = useCurrentList();
   const queryClient = useQueryClient();
-  const toastId = React.useRef<string | number | undefined>();
   const navigate = useNavigate();
 
-  const invalidateQueries = (queryKeys: QueryKey[]) => {
-    queryKeys.forEach((queryKey) =>
-      queryClient.invalidateQueries({ queryKey }),
-    );
-  };
-
-  const onError = (error: Error) => {
-    console.error(error);
-    toast.error(error.message ?? "Server Error", { id: toastId.current });
-  };
-
-  const onMutateMessage = (message: string) => {
-    toastId.current = toast.loading(message);
-  };
-
-  const toastSuccess = (message: string) => {
-    toast.success(message, { id: toastId.current });
-  };
-
-  async function optimisticUpdate<T extends object>(
-    queryKey: QueryKey,
-    updater: Updater<T, T>,
-  ): Promise<{ previous: T | null | undefined }> {
-    await queryClient.cancelQueries({ queryKey });
-    const previous = queryClient.getQueryData<T>(queryKey);
-    queryClient.setQueryData<T>(queryKey, (prev) => {
-      if (!prev) return prev;
-      if (typeof updater === "function") {
-        return updater(prev);
-      }
-      return updater;
-    });
-    return { previous };
-  }
-
-  function onErrorOptimistic<T extends object>(
-    queryKey: QueryKey,
-    context: { previous: T | null | undefined } | undefined,
-  ) {
-    if (context?.previous) {
-      queryClient.setQueryData(queryKey, context.previous);
-    }
-  }
+  const {
+    onError,
+    onMutateMessage,
+    toastSuccess,
+    optimisticUpdate,
+    onErrorOptimistic,
+    invalidateQueries,
+  } = useMutationHelpers();
 
   const deleteCategoryItem = useMutation({
     mutationFn: actions.deleteCategoryItem.orThrow,
