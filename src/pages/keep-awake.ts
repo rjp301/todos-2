120b30c1a87b06615b1ad909a36a522fe2dbb5c5
Riley Paul <rileypaul96@gmail.com>
2024-10-15 21:38:50 -0600

    add keep awake route

---
+++ src/pages/keep-awake.ts
@@ -0,0 +1,10 @@
+import type { APIRoute } from "astro";
+import { count, db, User } from "astro:db";
+
+export const GET: APIRoute = async (req) => {
+  const numUsers = await db
+    .select({ count: count() })
+    .from(User)
+    .then((rows) => rows[0].count);
+  return new Response(`There are ${numUsers} users in the database.`);
+};
