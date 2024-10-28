519ba3a37dba082ff1811f092cda2a1f945fe628
Riley Paul <rileypaul96@gmail.com>
2024-10-27 20:47:17 -0600

    upgrade astro

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Logo: React.FC = () => {
   return (
-    <a href="/" className="flex items-center gap-2 text-xl">
+    <a href="/" className="flex items-center gap-3 text-xl">
       <i className="fa-solid fa-earth text-primary" />
       <span className="font-bold">LighterTravel</span>
     </a>
