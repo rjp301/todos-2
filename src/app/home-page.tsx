6575fe515113c6cab2d56f3cb024c5ce05d34d8b
Riley Paul <rileypaul96@gmail.com>
2025-01-02 11:13:42 -0700

    convert homepage and feedback button

--- src/app/home-page.tsx
+++ src/app/home-page.tsx
@@ -1,9 +1,8 @@
 import { Plus } from "lucide-react";
 import React from "react";
 import AppHeader from "@/components/app-header";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import useMutations from "@/hooks/use-mutations";
+import { Button, Heading, Separator, Text } from "@radix-ui/themes";
 
 const HomePage: React.FC = () => {
   const { addList } = useMutations();
@@ -12,13 +11,23 @@ const HomePage: React.FC = () => {
       <AppHeader />
       <div className="container2 flex h-full items-center justify-center">
         <div className="flex h-full max-h-[50vh] flex-col items-center gap-2">
-          <i className="fa-solid fa-house-chimney mb-4 text-[3rem] text-primary" />
-          <h2 className="text-2xl font-bold">Welcome to LighterTravel</h2>
-          <p className="text-sm text-muted-foreground">
+          <i className="fa-solid fa-house-chimney text-accent-10 mb-4 text-[3rem]" />
+          <Heading as="h2" size="4">
+            Welcome to LighterTravel
+          </Heading>
+          <Text size="2" color="gray">
             Select a list to get packing
-          </p>
-          <Separator className="relative my-4">OR</Separator>
-          <Button variant="default" onClick={() => addList.mutate({})}>
+          </Text>
+          <Separator className="relative my-4" size="4">
+            <Text
+              size="2"
+              weight="medium"
+              className="bg-gray-1 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform px-2 text-xs text-muted-foreground"
+            >
+              OR
+            </Text>
+          </Separator>
+          <Button variant="soft" onClick={() => addList.mutate({})}>
             <Plus className="mr-2 size-4" />
             <span>Create a new list</span>
           </Button>
--- src/components/feedback-button.tsx
+++ src/components/feedback-button.tsx
@@ -1,21 +1,16 @@
 import React from "react";
 
-import { CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-
-import { Button } from "@/components/ui/button";
 import useMutations from "@/hooks/use-mutations";
 import { MessageSquare, Send } from "lucide-react";
-import { Textarea } from "@/components/ui/textarea";
+import {
+  Button,
+  Heading,
+  IconButton,
+  Popover,
+  Text,
+  TextArea,
+  Tooltip,
+} from "@radix-ui/themes";
 
 const FeedbackButton: React.FC = () => {
   const [value, setValue] = React.useState("");
@@ -23,33 +18,29 @@ const FeedbackButton: React.FC = () => {
   const { addFeedback } = useMutations();
 
   return (
-    <Tooltip>
-      <Popover
+    <Tooltip side="left" content="Leave feedback">
+      <Popover.Root
         open={open}
         onOpenChange={(isOpen) => {
           setOpen(isOpen);
           if (isOpen) setValue("");
         }}
       >
-        <PopoverTrigger asChild className="fixed bottom-6 right-6 z-50">
-          <TooltipTrigger asChild>
-            <Button
-              size="icon"
-              variant="secondary"
-              className="size-10 rounded-full border shadow-md"
-            >
-              <MessageSquare className="size-5" />
-            </Button>
-          </TooltipTrigger>
-        </PopoverTrigger>
-        <PopoverContent align="end" className="grid gap-4">
-          <CardHeader className="p-0">
-            <CardTitle>Leave Feedback</CardTitle>
-            <CardDescription>
+        <Popover.Trigger className="fixed bottom-6 right-6 z-50">
+          <IconButton variant="soft" radius="full" size="3">
+            <MessageSquare className="size-5" />
+          </IconButton>
+        </Popover.Trigger>
+        <Popover.Content align="end" className="grid gap-4">
+          <header>
+            <Heading size="3" weight="medium">
+              Leave Feedback
+            </Heading>
+            <Text size="2" color="gray">
               Want a feature added? Don't like something? Your feedback helps us
               improve the app for everyone 🚀
-            </CardDescription>
-          </CardHeader>
+            </Text>
+          </header>
           <form
             className="grid gap-2"
             onSubmit={(e) => {
@@ -58,20 +49,19 @@ const FeedbackButton: React.FC = () => {
               setValue("");
             }}
           >
-            <Textarea
+            <TextArea
               value={value}
               onChange={(e) => setValue(e.target.value)}
               rows={5}
               placeholder="Let's hear it"
             />
-            <Button type="submit">
+            <Button type="submit" variant="soft">
               <Send className="mr-2 size-4" />
               <span>Submit</span>
             </Button>
           </form>
-        </PopoverContent>
-      </Popover>
-      <TooltipContent side="left">Leave feedback</TooltipContent>
+        </Popover.Content>
+      </Popover.Root>
     </Tooltip>
   );
 };
