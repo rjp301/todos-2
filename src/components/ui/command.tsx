734bd6ca1d542a2a4a9363f1cc3527307a35a1eb
Riley Paul <rileypaul96@gmail.com>
2025-01-02 21:07:53 -0700

    tweak border colour

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -102,7 +102,7 @@ const CommandSeparator = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <CommandPrimitive.Separator
     ref={ref}
-    className={cn("-mx-1 h-px bg-gray-7", className)}
+    className={cn("-mx-1 h-px bg-gray-6", className)}
     {...props}
   />
 ));
