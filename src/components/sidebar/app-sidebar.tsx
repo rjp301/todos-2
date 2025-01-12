20433989b6556f009207acfd0cf5c17d16beb793
Riley Paul <rileypaul96@gmail.com>
2025-01-12 16:42:26 -0700

    start on collapsible sidebar

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -10,9 +10,13 @@ import { NAVBAR_HEIGHT } from "@/lib/constants";
 
 import UserAvatar from "../user-avatar";
 import Logo from "../logo";
+import { Button, IconButton } from "@radix-ui/themes";
+import { useAtom } from "jotai";
+import { sidebarOpenAtom } from "./store";
+import { cn } from "@/lib/utils";
 
 const AppSideBar: React.FC = () => {
-  // const { toggleSidebar } = useSidebar();
+  const [isOpen, setIsOpen] = useAtom(sidebarOpenAtom);
 
   // useEventListener("keydown", (e) => {
   //   if (getIsTyping() || getHasModifier(e)) return;
@@ -22,32 +26,45 @@ const AppSideBar: React.FC = () => {
   // });
 
   return (
-    <div
-      className="z-0 flex h-screen w-[20rem] p-2 outline-none"
-      style={
-        {
-          "--initial-transform": "calc(100% + 8px)",
-        } as React.CSSProperties
-      }
-    >
-      <div className="flex h-full w-full grow flex-col rounded-[16px] border bg-panel">
-        <header
-          className="flex items-center justify-between border-b px-4"
-          style={{ height: NAVBAR_HEIGHT }}
+    <div className="grid grid-cols-[1fr_auto] items-center gap-2 p-2">
+      <div className="overflow-hidden">
+        <div
+          className={cn(
+            "z-0 flex h-screen w-[20rem] outline-none",
+            !isOpen && "w-0",
+          )}
         >
-          <Logo />
-          <UserAvatar />
-        </header>
-        <ResizablePanelGroup autoSaveId="sidebar-panels" direction="vertical">
-          <ResizablePanel defaultSize={40}>
-            <PackingLists />
-          </ResizablePanel>
-          <ResizableHandle withHandle />
-          <ResizablePanel>
-            <PackingItems />
-          </ResizablePanel>
-        </ResizablePanelGroup>
+          <div className="relative flex h-full w-full grow flex-col rounded-4 border bg-panel">
+            <header
+              className="flex items-center justify-between border-b px-4"
+              style={{ height: NAVBAR_HEIGHT }}
+            >
+              <Logo />
+              <UserAvatar />
+            </header>
+            <ResizablePanelGroup
+              autoSaveId="sidebar-panels"
+              direction="vertical"
+            >
+              <ResizablePanel defaultSize={40}>
+                <PackingLists />
+              </ResizablePanel>
+              <ResizableHandle withHandle />
+              <ResizablePanel>
+                <PackingItems />
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </div>
+        </div>
       </div>
+      <IconButton
+        variant="ghost"
+        size="1"
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="w-4 h-1/2"
+      >
+        <i className="fa-solid fa-chevron-left text-1" />
+      </IconButton>
     </div>
   );
 };
