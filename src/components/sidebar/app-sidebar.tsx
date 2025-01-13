3a7e4c3d79d12f96c4e4e4a35a91851a606d5882
Riley Paul <rileypaul96@gmail.com>
2025-01-13 09:36:21 -0700

    get sidebar flying in and out

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -10,62 +10,79 @@ import { NAVBAR_HEIGHT } from "@/lib/constants";
 
 import UserAvatar from "../user-avatar";
 import Logo from "../logo";
-import { Button, IconButton } from "@radix-ui/themes";
 import { useAtom } from "jotai";
-import { sidebarOpenAtom } from "./store";
-import { cn } from "@/lib/utils";
+import { desktopSidebarOpenAtom, mobileSidebarOpenAtom } from "./store";
+import { cn, getHasModifier, getIsTyping } from "@/lib/utils";
+import { useEventListener } from "usehooks-ts";
+import { useIsMobile } from "@/hooks/use-is-mobile";
 
 const AppSideBar: React.FC = () => {
-  const [isOpen, setIsOpen] = useAtom(sidebarOpenAtom);
+  const isMobile = useIsMobile();
+  const [isOpen, setIsOpen] = useAtom(
+    isMobile ? mobileSidebarOpenAtom : desktopSidebarOpenAtom,
+  );
 
-  // useEventListener("keydown", (e) => {
-  //   if (getIsTyping() || getHasModifier(e)) return;
-  //   if (e.code === "KeyB") {
-  //     toggleSidebar();
-  //   }
-  // });
+  useEventListener("keydown", (e) => {
+    if (getIsTyping() || getHasModifier(e)) return;
+    if (e.code === "KeyB") {
+      setIsOpen((prev) => !prev);
+    }
+    if (isMobile && e.code === "Escape") {
+      setIsOpen(false);
+    }
+  });
 
   return (
-    <div className="grid grid-cols-[1fr_auto] items-center gap-2 p-2">
-      <div className="overflow-hidden">
+    <>
+      {!isMobile && (
         <div
           className={cn(
-            "z-0 flex h-screen w-[20rem] outline-none",
-            !isOpen && "w-0",
+            "transition-all duration-200 ease-in-out",
+            isOpen ? "w-[20rem]" : "w-0",
           )}
-        >
-          <div className="relative flex h-full w-full grow flex-col rounded-4 border bg-panel">
-            <header
-              className="flex items-center justify-between border-b px-4"
-              style={{ height: NAVBAR_HEIGHT }}
-            >
-              <Logo />
-              <UserAvatar />
-            </header>
-            <ResizablePanelGroup
-              autoSaveId="sidebar-panels"
-              direction="vertical"
-            >
-              <ResizablePanel defaultSize={40}>
-                <PackingLists />
-              </ResizablePanel>
-              <ResizableHandle withHandle />
-              <ResizablePanel>
-                <PackingItems />
-              </ResizablePanel>
-            </ResizablePanelGroup>
-          </div>
-        </div>
-      </div>
-      <IconButton
-        variant="ghost"
-        size="1"
-        onClick={() => setIsOpen((prev) => !prev)}
-        className="w-4 h-1/2"
+        />
+      )}
+      {isMobile && isOpen && (
+        <div
+          className="fixed inset-0 z-10 bg-panel-translucent backdrop-blur"
+          onClick={() => setIsOpen(false)}
+        />
+      )}
+      <div
+        className={cn(
+          "fixed bottom-0 left-0 top-0 z-20 w-[20rem] p-2 transition-transform duration-200 ease-in-out",
+          isOpen ? "translate-x-0" : "-translate-x-full",
+        )}
       >
-        <i className="fa-solid fa-chevron-left text-1" />
-      </IconButton>
-    </div>
+        <div className="relative flex h-full w-full flex-col rounded-4 border bg-panel-solid">
+          <header
+            className="flex items-center justify-between border-b px-4"
+            style={{ height: NAVBAR_HEIGHT }}
+          >
+            <Logo />
+            <UserAvatar />
+          </header>
+          <ResizablePanelGroup autoSaveId="sidebar-panels" direction="vertical">
+            <ResizablePanel defaultSize={40}>
+              <PackingLists />
+            </ResizablePanel>
+            <ResizableHandle withHandle />
+            <ResizablePanel>
+              <PackingItems />
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </div>
+        <button
+          onClick={() => setIsOpen((prev) => !prev)}
+          className={cn(
+            "absolute bottom-12 top-12 w-3 border bg-panel-solid opacity-0 transition-opacity duration-200 ease-in-out hover:opacity-100",
+            isOpen
+              ? "-right-1.5 rounded-full"
+              : "-right-3 rounded-r-full border-l-0",
+          )}
+        />
+      </div>
+    </>
   );
 };
 
--- src/components/sidebar/store.ts
+++ src/components/sidebar/store.ts
@@ -1,3 +1,10 @@
 import { atomWithStorage } from "jotai/utils";
 
-export const sidebarOpenAtom = atomWithStorage<boolean>("sidebarOpen", false);
+export const mobileSidebarOpenAtom = atomWithStorage<boolean>(
+  "mobileSidebarOpen",
+  false,
+);
+export const desktopSidebarOpenAtom = atomWithStorage<boolean>(
+  "desktopSidebarOpen",
+  false,
+);
